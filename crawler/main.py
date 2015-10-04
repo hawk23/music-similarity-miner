872d341e71843de3d9ca57910fc7f7a612416bb5
Mario <pleaselou@gmx.net>
2015-10-04 07:34:57 +0200

    handle artist names with special characters like '/'

--- crawler/main.py
+++ crawler/main.py
@@ -23,7 +23,11 @@ for line in artists:
     print 'fetched %s links for %s' % (len(rs), query)
     print 'downloading ...'
 
-    out_file = args.outputDir + "/" + line.rstrip()+ ".txt"
+    out_file_name = line.rstrip()
+    # only allow alphanumerric chars and spaces in filenames
+    out_file_name = "".join(x for x in out_file_name if x.isalnum() or x.isspace())
+    out_file = args.outputDir + "/" + out_file_name + ".txt"
+
     sp.download(rs, out_file)
 
     print 'saved to ' + out_file
